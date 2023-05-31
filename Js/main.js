df83d17f255ba9a536afab9e1f425d9206512950
AGUILARDEFREITES <aguilardefreites@gmail.com>
2023-05-31 20:36:19 -0300

    first commit

---
+++ Js/main.js
@@ -0,0 +1,40 @@
+//tarea 5 
+/*
+console.log("Hola Mundo");
+
+console.log("num1");
+console.log("num2");
+console.log("saludo");
+console.log("nombreCurso")
+
+let num1 = 50;
+let num2 = 10;
+let saludo = "¡Hola!";
+let nombreCurso = "Codigo LATAM";
+
+let resultadoSuma = num1 + num2;
+let resultadoResta = num1 - num2;
+let resultadoMultiplicacion = num1 * num2;
+
+console.log(resultadoSuma);
+console.log(resultadoResta);
+console.log(resultadoMultiplicacion);
+console.log(saludo + "Esto es " + nombreCurso);
+*/
+
+//tarea 6
+
+for(let i = 10; i > 0; i--){
+    console.log(i)
+}
+console.log("Despegamos!")
+
+
+
+
+
+
+
+
+
+
